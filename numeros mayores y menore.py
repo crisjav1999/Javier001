8350eab2cb2a409bc5bf66605b05bfaaeac8c27d
crisjav1999 <56598677+crisjav1999@users.noreply.github.com>
2019-10-22 12:37:06 -0500

    Add files via upload

---
+++ numeros mayores y menore.py
@@ -0,0 +1,8 @@
+number1 = int(input("Ingrese un numero A"))
+number2= int(input("Ingrese un numero B"))
+
+if number1 > number2:
+    print("numero A",number1," es Mayor que B",number2)
+else:
+    print("numero B", number2, "es Mayor que A",number1)
+ 
---
+++ taller.py
@@ -0,0 +1,13 @@
+number= int(input("Ingrese un numero"))
+
+if number >= 0 and number <=10:
+    print("esta entre [0;10]")
+    
+elif number >= 11 and number <= 20:
+    print("esta entre [11;20]")
+
+elif number >= 21 and number <= 30:
+    print("esta entre [21;30]")
+    
+else:
+    print("el valor está fuera de rango")
