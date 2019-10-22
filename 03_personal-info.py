26ce2256dad6660194009ce721f42bdb33421987
crisjav1999 <56598677+crisjav1999@users.noreply.github.com>
2019-10-22 10:05:49 -0500

    Add files via upload

---
+++ 03_personal-info.py
@@ -0,0 +1,18 @@
+nombre = input("¿Cual es tu nombre?")
+apellido = input("¿Cual es tu apellido?")
+
+print("Hey",nombre,apellido,"bienvenido al Motel Buenavista,"+"\n"+
+      "Ingrese los datos para su factura")
+
+ci= input("ingrese su cedula")
+telefono= input("ingrese su telefono")
+
+print("motel buenavista"+"\n"+
+      "factura a nombre de:",nombre,apellido+"\n"+
+      "Cedula de identidad:",ci+"\n"+
+      "Cedula de identidad:",telefono)
+      
+
+
+
+
